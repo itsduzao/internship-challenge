b794cada8e10b6392a8cecb12cabacd06c3b066a
Eduardo Henrique <itsduzao@outlook.com>
2024-11-02 19:43:49 -0300

    feat: implement lcm calculation API endpoint with error handling

--- mmc/views.py
+++ mmc/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from math import lcm
 
-# Create your views here.
+@api_view(['POST'])
+def calcular_mmc(request):
+  try:
+    try:
+      x = int(request.data.get('x'))
+      y = int(request.data.get('y'))
+    except ValueError:
+      return Response({'error': 'Os valores de x e y devem ser números inteiros'}, status=status.HTTP_400_BAD_REQUEST)
+
+    if x >= y:
+      return Response({'error': 'O valor de x deve ser menor que o valor de y'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if x <= 0 or y <= 0:
+      return Response({'error': 'Os valores de x e y devem ser maiores que 0'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    result = lcm(range(x, y+1))
+
+    return Response({'x': x, 'y': y, 'result': result}, status=status.HTTP_200_OK)
+  except:
+    return Response({'error': 'Ocorreu um erro ao calcular o MMC'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+  
