89b85fbeef8dbfa2489892541854401fe547670d
Eduardo Henrique <itsduzao@outlook.com>
2024-11-03 17:24:50 -0300

    feat: enhance input validation in calcular_mmc function to handle missing and invalid values

--- mmc/views.py
+++ mmc/views.py
@@ -5,22 +5,27 @@ from .modules.lcm_range import lcm_range
 
 @api_view(['POST'])
 def calcular_mmc(request):
-  try:
     try:
-      x = int(request.data.get('x'))
-      y = int(request.data.get('y'))
-    except ValueError:
-      return Response({'error': 'Os valores de x e y devem ser números inteiros'}, status=status.HTTP_400_BAD_REQUEST)
+      x = request.data.get('x')
+      y = request.data.get('y')
 
-    if x >= y:
-      return Response({'error': 'O valor de x deve ser menor que o valor de y'}, status=status.HTTP_400_BAD_REQUEST)
-    
-    if x <= 0 or y <= 0:
-      return Response({'error': 'Os valores de x e y devem ser maiores que 0'}, status=status.HTTP_400_BAD_REQUEST)
-    
-    result = lcm_range(x,y)
+      if x is None or y is None:
+        return Response({'error': 'Os valores de x e y são obrigatórios'}, status=status.HTTP_400_BAD_REQUEST)
+      
+      try:
+        x = int(request.data.get('x'))
+        y = int(request.data.get('y'))
+      except ValueError:
+        return Response({'error': 'Os valores de x e y devem ser números inteiros'}, status=status.HTTP_400_BAD_REQUEST)
+      
+      if x <= 0 or y <= 0:
+        return Response({'error': 'Os valores de x e y devem ser maiores que 0'}, status=status.HTTP_400_BAD_REQUEST)
+      
+      if x >= y:
+        return Response({'error': 'O valor de x deve ser menor que o valor de y'}, status=status.HTTP_400_BAD_REQUEST)
+      
+      result = lcm_range(x,y)
 
-    return Response({'x': x, 'y': y, 'result': result}, status=status.HTTP_200_OK)
-  except:
-    return Response({'error': 'Ocorreu um erro ao calcular o MMC'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-  
+      return Response({'x': x, 'y': y, 'result': result}, status=status.HTTP_200_OK)
+    except Exception as e:
+      return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
